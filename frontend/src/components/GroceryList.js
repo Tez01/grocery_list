4d92e468eb84b80618c2e8bebac2230942898a43
Tez01 <tejpartap02@gmail.com>
2022-10-18 14:37:14 -0400

    Dynamically add items to the list on add button click. Need to add unique keys in future.

--- frontend/src/components/GroceryList.js
+++ frontend/src/components/GroceryList.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import List from "./List";
 import Header from "./Header";
 import Utilities from "./Utilities";
 import "../../static/css/grocery_list.css";
 
 // React list element
-const reactListElement = (data) => {
+const reactListElement = (data, key) => {
   return (
-    <div className="list__element">
+    <div className="list__element" key={key}>
       <h3>{data}</h3>
       <button>Edit</button>
       <button>Delete</button>
@@ -28,7 +28,7 @@ function getReactComponents(listOfComponents) {
   const reactComponents = [];
   for (let i = 0; i < listOfComponents.length; i++) {
     /////////////// if data in list component is empty
-    reactComponents.push(reactListElement(listOfComponents[i]));
+    reactComponents.push(reactListElement(listOfComponents[i], i));
   }
   return reactComponents;
 }
@@ -43,14 +43,20 @@ function testGetReactComponents() {
   console.log(reactComponents);
 }
 const GroceryList = () => {
-  // Get current list items from database
-  const currentListItems = getCurrentItems();
+  // Create a state variable to add list elements dynamically
+  const [components, updateComponents] = useState([]);
 
-  // Convert these to React Components
-  const reactComponents = getReactComponents(currentListItems);
+  // This useEffect hook will only run once so as to fetch data from database,
+  // on first time this component is rendered.
+  useEffect(() => {
+    // Get current list items from database
+    const currentListItems = getCurrentItems();
 
-  // Create a state variable for lists, initialize with the current react components
-  const [components, updateComponents] = useState(reactComponents);
+    // Convert these to React Components
+    let reactComponents = Array.from(getReactComponents(currentListItems));
+    // Update the state variable for list components
+    updateComponents([...components, ...reactComponents]);
+  }, []);
 
   // Create a click handler that will update the component list at each button click for add button
   const submitHandler = (e) => {
@@ -62,18 +68,17 @@ const GroceryList = () => {
     if (textInput != "") {
       console.log(textInput);
       // Convert to react component
-      const reactComponent = reactListElement(textInput);
+      const reactComponent = reactListElement(textInput, 45);
       console.log(reactComponent);
       // Push to current array of components
-      reactComponents.push(reactComponent);
-      console.log(reactComponents);
+      let newArr = [...components, reactComponent];
+      console.log(newArr);
       // Update the state
-      updateComponents(<h1>Hello</h1>);
+      updateComponents(newArr);
 
       // Make a post request to the backend
     }
   };
-  testGetReactComponents();
   return (
     <div className="grocery-list container ">
       <Header />
