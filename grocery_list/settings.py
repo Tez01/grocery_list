e07cb8cdd4b9cd2a8937f7e540c20a3c7ec04503
Tez01 <tejpartap02@gmail.com>
2022-10-18 18:45:12 -0400

    Make debug false in settings.py

--- grocery_list/settings.py
+++ grocery_list/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c8udle&&ivqm^8$-q-z$6o0qb22gvg9jf8bg$9hz5supb%(_*c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['grocery-list-maker-app.herokuapp.com',
                  '127.0.0.1', 'localhost']
