9f0348e3df632ae6da739c906517b66fd678a231
Tez01 <tejpartap02@gmail.com>
2022-10-20 17:46:22 -0400

    Remove edit view from frontend

--- frontend/views.py
+++ frontend/views.py
@@ -5,8 +5,3 @@ from django.shortcuts import render, redirect
 
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
-
-
-def redirectToHome(request, *args, **kwargs):
-    print("Came here")
-    return redirect('/')
