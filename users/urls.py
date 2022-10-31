b9a035fa81628eafb202d493ae3d7b887e4a6d85
Tez01 <tejpartap02@gmail.com>
2022-10-31 14:36:41 -0400

    Retirieve changes on user branch to main branch

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 
+app_name = "users"
 urlpatterns = [
     path('', views.index, name="index"),
     path('login/', views.login_view, name="login"),
